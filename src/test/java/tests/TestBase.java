c0d698cbb644f3bb9d76128f35d66a3924630ee3
Nurchik3009 <kaliakbarova3009@gmail.com>
2026-07-24 18:56:20 +0500

    Fix remote configuration in TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -42,7 +42,7 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
 
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
